6b9cfb963bc39d040ccd6893dbfc5822dd92a35a
ArtsiomFortunatov <artsiom@admin.com>
2021-10-30 20:37:27 +0300

    terraform conf

--- terraform/createtestinstance.tf
+++ terraform/createtestinstance.tf
@@ -7,12 +7,15 @@ resource "aws_key_pair" "ec2key" {
   public_key = "${file(var.public_key_path)}"
 }
 resource "aws_instance" "testInstance" {
+  tags = {
+    Name = "gitops_test"
+  }
   ami           = "${var.instance_ami}"
   instance_type = "${var.instance_type}"
   subnet_id = "${aws_subnet.subnet_public.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
   key_name = "${aws_key_pair.ec2key.key_name}"
- tags = {
+  tags = {
   Environment = "${var.environment_tag}"
  }
 }
